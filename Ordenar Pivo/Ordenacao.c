ae94204da09738077bf73a9be583695caa9b1fa5
Leonardo-Campos-2007 <leonardogomescampos2007@gmail.com>
2025-09-16 11:46:22 -0300

    add

--- Ordenar Pivo/Ordenacao.c
+++ Ordenar Pivo/Ordenacao.c
@@ -2,29 +2,54 @@
 #include <stdlib.h>
 #include <time.h>
 
-void TrocarElemento(int * A, int * B);
+void TrocarElemento(int *A, int *B);
 
 int Particao(int *V, int Inf, int Sup);
 
-int main(){
+int main()
+{
 
     int Vetor[] = {3, 6, 4, 5, 1, 7, 2};
     int Tamanho = sizeof(Vetor) / sizeof(int);
     Particao(Vetor, 1, 4);
-
 }
 
-void TrocarElemento(int * A, int * B){
+void TrocarElemento(int *A, int *B)
+{
 
-
-
-int temp = *A;
-*A = *B;
-*B = temp;
-
+    int temp = *A;
+    *A = *B;
+    *B = temp;
 }
 
-int Particao(int *V, int Inf, int Sup){
+int Particao(int *V, int Inf, int Sup)
+{
     int Pivot = V[(Inf + Sup) / 2];
-    printf("Pivot: ");
+    int i = Inf;
+    int j = Sup;
+
+    while (i <= j)
+    {
+        while (V[i] < Pivot)
+        {
+            i++;
+        }
+
+        while (V[j] > Pivot)
+        {
+            j--;
+        }
+
+        if (i <= j)
+        {
+            TrocarElemento(&V[i], &V[j]);
+            i++;
+            j--;
+        }
+    }
+
+    return i;
 }
+
+
+
