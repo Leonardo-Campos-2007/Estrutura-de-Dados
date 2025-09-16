84df325e8dc27225954894bed2825f795b8079ec
Leonardo-Campos-2007 <leonardogomescampos2007@gmail.com>
2025-09-16 12:04:11 -0300

    add

--- Ordenar Pivo/Ordenacao.c
+++ Ordenar Pivo/Ordenacao.c
@@ -6,12 +6,17 @@ void TrocarElemento(int *A, int *B);
 
 int Particao(int *V, int Inf, int Sup);
 
+void QuickSort(int *V, int Inf, int Sup);
+
+void ExibirVetor(int *V, int N);
+
 int main()
 {
 
     int Vetor[] = {3, 6, 4, 5, 1, 7, 2};
     int Tamanho = sizeof(Vetor) / sizeof(int);
-    Particao(Vetor, 1, 4);
+    Particao(Vetor, 0, Tamanho - 1);
+    ExibirVetor(Vetor, Tamanho);
 }
 
 void TrocarElemento(int *A, int *B)
@@ -48,8 +53,36 @@ int Particao(int *V, int Inf, int Sup)
         }
     }
 
+    // for (int p = Inf; p < i; p++)
+    // {
+    //     printf("%d\t", V[i]);
+    // }
+
+    // printf("\n[%d]\n", V[i]);
+
+    // for (int q = i + 1; q < i; q++)
+    // {
+    //     /* code */
+    // }
+
     return i;
 }
 
+void QuickSort(int *V, int Inf, int Sup)
+{
+    if (Inf < Sup)
+    {
+        int P = Particao(V, Inf, Sup);
+        QuickSort(V, Inf, P - 1);
+        QuickSort(V, P, Sup);
+    }
+}
 
-
+void ExibirVetor(int *V, int N)
+{
+    for (int i = 0; i < N; i++)
+    {
+        printf("%d\t", V[i]);
+    }
+    printf("\n");
+}
