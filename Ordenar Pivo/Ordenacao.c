5a6a1942a3c7c5dee250939b1de7b5bf32132038
Leonardo-Campos-2007 <leonardogomescampos2007@gmail.com>
2025-09-16 12:36:16 -0300

    add

--- Ordenar Pivo/Ordenacao.c
+++ Ordenar Pivo/Ordenacao.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define TAMANHO 150000
+
 void TrocarElemento(int *A, int *B);
 
 int Particao(int *V, int Inf, int Sup);
@@ -13,10 +15,28 @@ void ExibirVetor(int *V, int N);
 int main()
 {
 
-    int Vetor[] = {3, 6, 4, 5, 1, 7, 2};
+    int Vetor[] = TAMANHO;
     int Tamanho = sizeof(Vetor) / sizeof(int);
     QuickSort(Vetor, 0, Tamanho - 1);
     ExibirVetor(Vetor, Tamanho);
+
+    int Conjunto[TAMANHO];
+
+    srand(time(NULL));
+
+    for (int i = 0; i < TAMANHO; i++)
+    {
+        Conjunto[i] = rand() % 1000;
+    }
+
+    int Tamanho = sizeof(Conjunto) / sizeof(int);
+
+    clock_t T0 = clock();
+    InsertonSort(Conjunto, Tamanho);
+    clock_t TF = clock();
+
+    printf("Tempo de CPU: %f\n", (double)(TF - T0)/(CLOCKS_PER_SEC) );
+    ExibirVetor(Conjunto, Tamanho);
 }
 
 void TrocarElemento(int *A, int *B)
